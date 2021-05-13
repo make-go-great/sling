7a4daf33a51dcf22e7e486ad58eb2cf611fbcea6
Nguyen Tran Hau <hauvipapro@gmail.com>
2021-05-13 18:13:43 +0700

    test: test tables for Clone()

--- sling_test.go
+++ sling_test.go
@@ -1,6 +1,7 @@
 package sling
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,11 +14,48 @@ func TestNew(t *testing.T) {
 }
 
 func TestSlingClone(t *testing.T) {
-	s := New(nil)
-	clonedS, err := s.Clone()
-	assert.NoError(t, err)
-	assert.Equal(t, s.method, clonedS.method)
-	assert.Equal(t, s.reqURL, clonedS.reqURL)
-	assert.Equal(t, s.header, clonedS.header)
-	assert.Equal(t, s.queries, clonedS.queries)
+	tests := []struct {
+		name string
+		s    *Sling
+	}{
+		{
+			name: "method",
+			s: &Sling{
+				method:  "method",
+				header:  make(http.Header),
+				queries: []interface{}{},
+			},
+		},
+		{
+			name: "header",
+			s: &Sling{
+				header: http.Header{
+					"a": {"1", "2"},
+				},
+				queries: []interface{}{},
+			},
+		},
+		{
+			name: "queries",
+			s: &Sling{
+				header: make(http.Header),
+				queries: []interface{}{
+					"a", 1, true,
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			clonedS, err := tc.s.Clone()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.s.method, clonedS.method)
+			assert.Equal(t, tc.s.reqURL, clonedS.reqURL)
+			assert.Equal(t, tc.s.header, clonedS.header)
+			assert.Equal(t, tc.s.queries, clonedS.queries)
+			assert.Equal(t, tc.s.bodyProvider, clonedS.bodyProvider)
+			assert.Equal(t, tc.s.rspDecoder, clonedS.rspDecoder)
+		})
+	}
 }
